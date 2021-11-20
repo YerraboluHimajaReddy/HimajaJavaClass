73b5354a6e966528d77c88f432a1f261e1a1dfaa
Sony <Sony@Sony-PC>
2021-11-20 08:41:31 +0530

    Commit latest branch

---
+++ NewMavenProject/src/test/java/Sample/SampleProgram1.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class SampleProgram1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Welcome Himaja Reddy Yerrabolu");
+	}
+
+}
